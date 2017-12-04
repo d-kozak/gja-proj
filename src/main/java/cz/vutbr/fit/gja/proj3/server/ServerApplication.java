450cb7ad1670963a7051d77ee9d5a58f40db67cc
dkozak <dkozak94@gmail.com>
2017-12-04 14:39:32 +0100

    init minimum spring boot project

---
+++ src/main/java/cz/vutbr/fit/gja/proj3/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package cz.vutbr.fit.gja.proj3.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
---
+++ src/test/java/cz/vutbr/fit/gja/proj3/server/ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.vutbr.fit.gja.proj3.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
