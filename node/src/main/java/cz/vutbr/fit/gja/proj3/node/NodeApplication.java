f88615d4daf8fa6999f317d7a6bc86ccf45d2315
dkozak <dkozak94@gmail.com>
2017-12-04 17:51:02 +0100

    restructure the repository to consist of two modules - server and node

---
+++ node/src/main/java/cz/vutbr/fit/gja/proj3/node/NodeApplication.java
@@ -0,0 +1,12 @@
+package cz.vutbr.fit.gja.proj3.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeApplication.class, args);
+    }
+}
---
+++ node/src/test/java/cz/vutbr/fit/gja/proj3/node/NodeApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.vutbr.fit.gja.proj3.node;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
