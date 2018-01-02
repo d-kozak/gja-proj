73c4a9d29e1f6f45acfb16290b5cab6eb2c5f53e
Pheat <pheate@gmail.com>
2018-01-02 21:26:27 +0100

    Maybe something is wrong

--- server/src/main/java/cz/vutbr/fit/gja/proj3/server/project/boundary/ProjectRepository.java
+++ server/src/main/java/cz/vutbr/fit/gja/proj3/server/project/boundary/ProjectRepository.java
@@ -1,16 +1,16 @@
 package cz.vutbr.fit.gja.proj3.server.project.boundary;
 
-import cz.vutbr.fit.gja.proj3.server.node.entity.Node;
-import cz.vutbr.fit.gja.proj3.server.processing_task.entity.ProcessingTask;
 import cz.vutbr.fit.gja.proj3.server.project.entity.Project;
 import java.util.List;
 import javax.transaction.Transactional;
 import org.hibernate.Hibernate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Transactional
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     
-    @Transactional
     default List<Project> findAllEagerFetch() {
         List<Project> all = findAll();
         all.forEach(project -> Hibernate.initialize(project.getProcessingTasks()));
