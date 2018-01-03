2306284a9b0c9b863d4ec44b14117b0984c1c50d
dkozak <dkozak94@gmail.com>
2018-01-03 21:15:01 +0100

    prevent duplicating nodes by making node url it's private key in db and test this before inserting

--- server/src/main/java/cz/vutbr/fit/gja/proj3/server/node/boundary/NodeConverter.java
+++ server/src/main/java/cz/vutbr/fit/gja/proj3/server/node/boundary/NodeConverter.java
@@ -1,44 +1,44 @@
 package cz.vutbr.fit.gja.proj3.server.node.boundary;
 
 import cz.vutbr.fit.gja.proj3.server.node.entity.Node;
+import lombok.extern.java.Log;
+import org.ocpsoft.rewrite.el.ELBeanName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
-import lombok.extern.java.Log;
-import org.ocpsoft.rewrite.el.ELBeanName;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Log
 @ManagedBean(name = "nodeConverter")
 @Component(value = "nodeConverter")
 @ELBeanName(value = "nodeConverter")
 @RequestScoped
-@FacesConverter(value="nodeConverter", forClass=Node.class)
-public class NodeConverter implements Converter {  
+@FacesConverter(value = "nodeConverter", forClass = Node.class)
+public class NodeConverter implements Converter {
     @Autowired
     private NodeRepository repository;
-    
+
     @Override
     public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
         if (string != null && !string.isEmpty()) {
-            Node node = this.repository.findOne(Long.parseLong(string));
+            Node node = this.repository.findOne(string);
             return node;
-        }else{
+        } else {
             return null;
         }
     }
 
     @Override
-    public String getAsString(FacesContext fc, UIComponent uic, Object o) {        
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
         if (o != null) {
-            Node node = (Node)o;
-            return Long.toString(node.getId());
-        }else{
+            Node node = (Node) o;
+            return node.getUrl();
+        } else {
             return "";
         }
     }
--- server/src/main/java/cz/vutbr/fit/gja/proj3/server/node/boundary/NodeRepository.java
+++ server/src/main/java/cz/vutbr/fit/gja/proj3/server/node/boundary/NodeRepository.java
@@ -3,24 +3,27 @@ package cz.vutbr.fit.gja.proj3.server.node.boundary;
 import cz.vutbr.fit.gja.proj3.server.node.entity.Node;
 import org.hibernate.Hibernate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @Transactional
-public interface NodeRepository extends JpaRepository<Node, Long> {
+public interface NodeRepository extends JpaRepository<Node, String> {
 
     List<Node> findAllByActiveIsTrue();
+
     List<Node> findAllByActiveIsFalse();
 
     default List<Node> findAllEagerFetch() {
         List<Node> all = findAll();
         all.forEach(node -> Hibernate.initialize(node.getProcessingTasks()));
         all.forEach(
-            node -> node.getProcessingTasks().forEach(
-                    task -> Hibernate.initialize(task.getProcessingTaskUnits())
-            )
+                node -> node.getProcessingTasks()
+                            .forEach(
+                                    task -> Hibernate.initialize(task.getProcessingTaskUnits())
+                            )
         );
         return all;
     }
